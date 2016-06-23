c63d08ec22d7887fc8f39c6a0ba74bccaf3a2e10
Partylov <partylov@mail.ru>
2016-06-23 18:12:12 +0600

    Create MainActivity class and layout

---
+++ app/src/main/java/com/partylov/configurecontroller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.partylov.configurecontroller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dmitry on 23.06.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
