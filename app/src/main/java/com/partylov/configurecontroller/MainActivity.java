9b716ecb536b98f00dad4dbc296190b3533a115e
Partylov <partylov@mail.ru>
2016-06-24 16:02:38 +0600

    Added toolbar and search button.

--- app/src/main/java/com/partylov/configurecontroller/MainActivity.java
+++ app/src/main/java/com/partylov/configurecontroller/MainActivity.java
@@ -1,15 +1,30 @@
 package com.partylov.configurecontroller;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
-/**
- * Created by Dmitry on 23.06.2016.
- */
+
 public class MainActivity extends Activity {
+    private  Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        initToolbar();
+    }
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
