65de04603151bab2adaaf3d79fa2fc97b63bdf82
Partylov <partylov@mail.ru>
2016-06-24 17:57:23 +0600

    Added NavigationView.

--- app/src/main/java/com/partylov/configurecontroller/MainActivity.java
+++ app/src/main/java/com/partylov/configurecontroller/MainActivity.java
@@ -4,14 +4,16 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private  Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
         initToolbar();
